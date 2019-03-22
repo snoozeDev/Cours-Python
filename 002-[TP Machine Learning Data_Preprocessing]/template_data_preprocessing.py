889222007aa332ecea6d3d9cdc1f07f304088102
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-22 11:05:39 +0100

    data preproc

--- 002-[TP Machine Learning Data_Preprocessing]/template_data_preprocessing.py
+++ 002-[TP Machine Learning Data_Preprocessing]/template_data_preprocessing.py
@@ -9,3 +9,8 @@ Created on Fri Mar 22 11:00:00 2019
 # import libraries
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# import dataset
+df = pd.read_csv("Data.csv")
